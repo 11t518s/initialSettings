51d1b7121a2ba988d656a54d1ab25bd1b8dda0ca
11t518s <11t518s20161759@gmail.com>
2021-10-29 11:37:44 +0900

    initial react with typescript

---
+++ layouts/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      {' '}
+      package.json name 수정, index.html 수정 <br /> path @로 하는거 수정해주려면 tsconfig.json 이랑 wepack.config.ts둘다 수정해줘야됨
+    </div>
+  );
+};
+
+export default App;
