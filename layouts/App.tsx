7af3559cd2779dc36d677b0d5c251b9708ba5e68
11t518s <69836010+11t518s@users.noreply.github.com>
2021-10-29 13:39:37 +0900

    Update App.tsx

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
+import loadable from '@loadable/component'; // code spliting
 
 const App = () => {
   return <div>코딩힘내라 최봉수....</div>;
